b76e0178915d116ecdda0bcb759263331d4fd7f4
Jorge Chavez <chavezjorgeeduardo19@gmail.com>
2025-12-15 19:31:20 -0600

    libraries modified, missing characters in functions

--- script/mocks/deploy_collateral.py
+++ script/mocks/deploy_collateral.py
@@ -1,8 +1,11 @@
-from src.mocks import MockToken
-from vyper import VyperContract
+from src.mocks import mock_token
+from moccasin.boa_tools import VyperContract
 
 def deploy_collateral() -> VyperContract:
-    return MockToken.deploy()
+    print("Deploying token...")
+    mock_token_contract = mock_token.deploy()
+    print(f"Deployed Mock Token contract at {mock_token_contract.address}")
+    return mock_token_contract
 
 def moccasin_main():
-    return deploy_collateral
+    return deploy_collateral()
